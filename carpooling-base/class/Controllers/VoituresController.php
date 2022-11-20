7e2091277227ad6f1c2abe46370772606129db07
Nathan1Waxin <nathan.waxin@etu.unilim.fr>
2022-11-20 11:49:10 +0100

    rajout fichier VoituresCOntroller.php

---
+++ carpooling-base/class/Controllers/VoituresController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Voiture;
+
+class VoitureController
+{
+    /**
+     * Return the html for the create action.
+     */
+    public function createVoiture(): string
+    {
+        $html = '';
+
+        // If the form have been submitted :
+        if (isset($_POST['model']) &&
+            isset($_POST['couleur']) &&
+            isset($_POST['vitesseMax'])) {
+            // Create the user :
+            $voituresService = new VoituresService();
+            $isOk = $voituresService ->setVoiture(
+                null,
+                $_POST['model'],
+                $_POST['couleur'],
+                $_POST['vitesseMax']
+            );
+            if ($isOk) {
+                $html = 'Voiture créé avec succès.';
+            } else {
+                $html = 'Erreur lors de la création de la voiture.';
+            }
+        }
+
+        return $html;
+    }
+
+    /**
+     * Return the html for the read action.
+     */
+    public function getVoiture(): string
+    {
+        $html = '';
+
+        // Get all voitures :
+        $VoituresService = new VoituresService();
+        $voitures = $VoituresService -> getVoitures();
+
+        // Get html :
+        foreach ($voitures as $voitures) {
+            $html .=
+                '#' . $voitures->getId() . ' ' .
+                $voitures->getModel() . ' ' .
+                $voitures->getCouleur() . ' ' .
+                $voitures->getVitesseMax() . '<br />';
+        }
+
+        return $html;
+    }
+
+    /**
+     * Update the voiture.
+     */
+    public function updateVoiture(): string
+    {
+        $html = '';
+
+        // If the form have been submitted :
+        if (isset($_POST['id']) &&
+            isset($_POST['model']) &&
+            isset($_POST['couleur']) &&
+            isset($_POST['vitesseMax'])) {
+            // Update the voiture :
+            $VoitureService = new VoitureService();
+            $isOk = $VoitureService->setVoiture(
+                $_POST['id'],
+                $_POST['model'],
+                $_POST['couleur'],
+                $_POST['vitesseMax']
+            );
+            if ($isOk) {
+                $html = 'Voiture mis à jour avec succès.';
+            } else {
+                $html = 'Erreur lors de la mise à jour de la voiture.';
+            }
+        }
+
+        return $html;
+    }
+
+    /**
+     * Delete an Voiture.
+     */
+    public function deleteUser(): string
+    {
+        $html = '';
+
+        // If the form have been submitted :
+        if (isset($_POST['id'])) {
+            // Delete the user :
+            $usersService = new UsersService();
+            $isOk = $usersService->deleteUser($_POST['id']);
+            if ($isOk) {
+                $html = 'Utilisateur supprimé avec succès.';
+            } else {
+                $html = 'Erreur lors de la supression de l\'utilisateur.';
+            }
+        }
+
+        return $html;
+    }
+}
