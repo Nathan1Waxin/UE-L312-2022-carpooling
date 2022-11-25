ec6e5597001dcf2ec8bdbd54aacbc26b5a062f8c
Nathan1Waxin <nathan.waxin@etu.unilim.fr>
2022-11-25 12:50:24 +0100

    rajout du <label> reservation

--- carpooling-base/vues/vues_users/users_create.php
+++ carpooling-base/vues/vues_users/users_create.php
@@ -2,6 +2,7 @@
 
 use App\Controllers\UsersController;
 use App\Services\VoituresService;
+use App\Services\ReservationsService;
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -27,6 +28,13 @@ echo $controller->createUser();
     <?php foreach ($voitures as $voiture): ?>
         <?php $voitureName = $car->getBrand() . ' ' . $voiture->getModel() . ' ' . $voiture->getColor(); ?>
         <input type="checkbox" name="cars[]" value="<?php echo $voiture->getId(); ?>"><?php echo $voitureName; ?>
+        <br />
+    <?php endforeach; ?>
+    <br />
+    <label for="reservation">Contrat(s) :</label>
+    <?php foreach ($reservations as $reservation): ?>
+        <?php $reservationName = $reservations->getName_client() . ' ' . $reservations->getTele_client() . ' ' . $reservations->getMail_client(); ?>
+        <input type="checkbox" name="reservations[]" value="<?php echo $reservation->getId(); ?>"><?php echo $reservationName; ?>
         <br />
     <?php endforeach; ?>
     <br />
